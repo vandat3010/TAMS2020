0f4bdcc2e8f51f077b6f8a1fb05d9799d9a28647
vandat3010 <vandat1095@gmail.com>
2020-08-02 23:57:05 +0700

    commit all

---
+++ TAMS/Content/CategoryNews.js
@@ -0,0 +1,38 @@
+﻿$(document).ready(function () {
+    var newsPage = new NewsPage();
+    newsPage.init();
+});
+class NewsPage {
+    constructor() {
+    }
+    init() {
+
+        $(".page-link").on("click", this.clickPage);
+    }
+    clickPage() {
+        var CategoryId = $("#CategoryIdNow").text();
+        $.ajax({
+            url: "/HomeNews/GetNewsOfPageCategory?CategoryId=" + CategoryId+"&&Page=" + this.id,
+            dataType: "json",
+            method: "GET",
+        }).done((data) => {
+            $("#more-news").empty();
+            data.Item1.map((news) => {
+
+                $("#more-news").append(`<div class="row m-b-20">
+                            <div class="w-100">
+                                <img style="float: left;margin-right: 20px" src="../../Content/imgs/`+ news.Avatar + `" width="220" />
+                                <div class="text-secondary text-14px">`+ news.ModifyDate + `</div>
+                                <h6>`+ news.Name + `</h6>
+                                <p class="text-14px ">`+ news.Sapo + `</p>
+                            </div>
+                        </div>`)
+            });
+            $(".page-link").removeClass("bg-primary text-white");
+            $("#" + this.id).addClass("bg-primary text-white");
+
+        }).fail(() => {
+            console.log("error");
+        })
+    }
+}
